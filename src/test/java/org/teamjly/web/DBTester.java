6a4764a947b65cf099c2a0b7a329e10d8185ea63
juyoungjung <juyoungjung@172.30.121.89>
2017-01-15 16:37:45 +0900

    update test

---
+++ src/test/java/org/teamjly/web/DBTester.java
@@ -0,0 +1,31 @@
+package org.teamjly.web;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
+public class DBTester {
+
+
+	@Test
+	public void dbTest() throws Exception{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection(
+				"jdbc:mysql://172.30.121.78:3306/candle?useSSL=false&serverTimezone=Asia/Seoul", "makemoney",
+				"makemoney");
+		System.out.println(con);
+		con.close();
+	}
+	
+	
+	
+}
